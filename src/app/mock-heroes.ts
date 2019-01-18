58e2723dd9743b78ffd4cf2d11178ef223d9d170
MelanieRotolo <melanie.rotolo@devinci.fr>
2019-01-18 01:36:14 +0100

    2. Display a List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Xerath' },
+  { id: 2, name: 'Janna' },
+  { id: 3, name: 'Soraka' },
+  { id: 4, name: 'Rakan' },
+  { id: 5, name: 'Teemo' },
+  { id: 6, name: 'Morgana' },
+  { id: 7, name: 'Thresh' },
+  { id: 8, name: 'Lux' },
+  { id: 9, name: 'Orianna' },
+  { id: 0, name: 'Yasuo' }
+];
